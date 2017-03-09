8bb5927bc619899e4c4b19a48a44b874dda6ca36
Steve Mayszak <stevemayszak@gmail.com>
2017-03-09 04:47:32 +0000

    first commit of simple health monitor

---
+++ HealthMonitor/Controllers/HostDetailController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthMonitor.Controllers
+{
+    [Route("api/[controller]")]
+    public class HostDetailController : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            var result = new JsonResult(
+
+                    new
+                    {
+                        HostName = Environment.MachineName,
+                        IpAddress = Request.HttpContext.Connection.RemoteIpAddress.ToString()
+                    }
+                );
+            return result;
+        }
+    }
+}
---
+++ HealthMonitor/Controllers/HostStatusController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthMonitor.Controllers
+{
+    [Route("api/[controller]")]
+    public class HostStatusController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "alive";
+        }
+    }
+}
